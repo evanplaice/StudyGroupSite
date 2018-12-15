6364c3b3e04d09eb429e657b9aea18777c3280fa
Badr Choubai <badr.choubai1048@gmail.com>
2018-12-15 16:50:08 -0700

    Change links in Nav to ones suggest on github issue

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,15 +7,20 @@ const Navbar = () => {
   return (
     <NavbarStyles>
       <ul>
-        <li>About</li>
+        <li>Group Learning</li>
         <li>Projects</li>
       </ul>
 
       <LogoIcon height="100" width="100" />
 
       <ul>
-        <li>Resources</li>
-        <li>Group Learning</li>
+        <li>About</li>
+        <li>Get Involved</li>
+        <li>
+          <a href="http://slack.bootcamperscollective.com/">
+            Slack
+          </a>
+        </li>
       </ul>
       <button className="hamburger">Menu</button>
     </NavbarStyles>
