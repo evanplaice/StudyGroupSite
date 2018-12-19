7fe5220655625051a7450db1af1e181e64abee17
Joey Ricketts <joeyr84@gmail.com>
2018-12-18 19:48:33 -0700

    added a mockup

--- src/components/Home/AboutHome.js
+++ src/components/Home/AboutHome.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import styled from "@emotion/styled";
+import { Link } from "@reach/router";
 
 export default class AboutHome extends Component {
   render() {
     return (
       <Container>
         <Title>About</Title>
+        <Par>
+          We are a group of beginner to mid-level developers who get together to
+          help each other learn, work on open-source projects, and hangout. To
+          learn more about what we are about, what we do, or how to get involved
+          click <Link to="/About">here</Link>
+        </Par>
       </Container>
     );
   }
@@ -17,9 +24,16 @@ const Title = styled.h2`
 `;
 const Container = styled.div`
   height: 100vh;
-  width: 100vw;
+  width: 100%;
   background-color: #000;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+const Par = styled.p`
+  font-size: 20px;
+  color: #fff;
+  padding-left: 10%;
+  padding-right: 10%;
 `;
