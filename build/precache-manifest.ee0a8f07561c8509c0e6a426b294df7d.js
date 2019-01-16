545ca8ffc70407fcbe70e5649a134aa03322cf69
Joey Ricketts <joeyr84@gmail.com>
2019-01-15 19:07:02 -0700

    built

---
+++ build/precache-manifest.ee0a8f07561c8509c0e6a426b294df7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3b8e4830af1d654166a0756c955c1e2",
+    "url": "/static/media/bclogo.e3b8e483.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b21112ded54bec3d5ab",
+    "url": "/static/js/main.6b21112d.chunk.js"
+  },
+  {
+    "revision": "c8fa59b141f9fcf8c365",
+    "url": "/static/js/1.c8fa59b1.chunk.js"
+  },
+  {
+    "revision": "6b21112ded54bec3d5ab",
+    "url": "/static/css/main.3986a28f.chunk.css"
+  },
+  {
+    "revision": "0b7f5ede0326dba949ce59c46e1dcbd8",
+    "url": "/index.html"
+  }
+];
